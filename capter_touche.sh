81cf9116af7011d06df55bb1bf6674cd656a5f5e
Léopold <leopold.chappuis@etu.utc.fr>
2022-07-07 19:24:06 +0200

    ajout des conditions d'arrets de la fonction
    Si on presse la touche "e" pour valider le choix de la case ou la touche "a" pour poser un flag alors on arrete la fonction qui récupère les touches

--- capter_touche.sh
+++ capter_touche.sh
@@ -11,7 +11,15 @@ getkey() {
 		elif [ "$input" == "q" ]; then
 			echo "à gauche"
 		elif [ "$input" == "d" ]; then
-			echo "à droite" 
+			echo "à droite"
+		elif [ "$input" == "e" ]; then
+			echo "découvre la touche"
+			#action pour découvrir la case en question
+			exit
+		elif [ "$input" == "a" ]; then
+			echo "poser un flag"
+		 	#action pour poser le flag 
+			exit
 		else
 			getkey
 		fi
