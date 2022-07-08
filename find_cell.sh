6ba2c327f2007cbaa5bc5c6b856330e270b657bd
Jules Boulet <jules.boulet@etu.utc.fr>
2022-07-08 10:02:21 +0200

    Ajout de la fonction find_coord
    Cette fonction renvoie les coordonnées à partir de l'index du tableau founi.

--- find_cell.sh
+++ find_cell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function (){
+function find_number(){
 	grid=$1
 	x=$2
 	y=$3
@@ -8,3 +8,14 @@ function (){
 	res=$(($x + $y * sqrt($len_grid)))
 	echo $res
 }
+
+function find_coord(){
+	grid=$1
+	loc=$2
+	len_grid=${#tableau_indi[@]}
+	sq_len=$((sqrt($len_grid)))
+	x=$(($loc%$sq_len))
+	y=$(($log / $sq_len))
+	echo $x $y	
+}
+
