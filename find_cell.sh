c448d304bdfec32e8620e357f3716ead3be7ee68
Jules Boulet <jules.boulet@etu.utc.fr>
2022-07-08 13:05:57 +0200

    Modification et débug de find_cell
    Les 3 fonctions disponibles dans find_cell fonctionnent, elles permettent de faire coord<->index et d'avoir les 8 voisins d'un index.

--- find_cell.sh
+++ find_cell.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
 find_number(){
-	local x=$1
-	local y=$2
-	local len_grid=${#grid[@]}
-	local res=$(($x + $y * sqrt($len_grid)))
-	echo $res
+	local x="$1"
+	local y="$2"
+	local len_grid="${#grid[@]}"
+	local float_sq_len=$(bc -l <<<"sqrt($len_grid)")
+	local sq_len=$(echo $float_sq_len | cut -d. -f1)
+	local res=$(($x + $y * $sq_len ))
+	echo "$res"
 }
 
 find_coord(){
 	local loc=$1
 	local len_grid=${#grid[@]}
-	local sq_len=$((sqrt($len_grid)))
+	local float_sq_len=$(bc -l <<<"sqrt($len_grid)")
+	local sq_len=$(echo $float_sq_len | cut -d. -f1)
 	local x=$(($loc%$sq_len))
-	local y=$(($log / $sq_len))
+	local y=$(($loc/$sq_len))
 	echo $x $y	
 }
 
-find_neig_sphere(){
-	local grid=
+find_neig(){
 	local len_grid=${#grid[@]}
-	local sq_len=$((sqrt($len_grid)))
+	local float_sq_len=$(bc -l <<<"sqrt($len_grid)")
+	local sq_len=$(echo $float_sq_len | cut -d. -f1)
 	local index=$1
-	local res=$(echo find_coord grid index)
+	local res=$(find_coord index)
 	local x=$(echo $res | cut -d" " -f1)
 	local y=$(echo $res | cut -d" " -f2)
 	
 	set -a neigs
 	
 	local n0=$((index-1-$sq_len))
-	if [ [ $n0 -lt 0 ] || [ $n0 -lt $(( $(($y-1)) * $sq_len)) ]
+	if [[ $n0 -lt 0 || $n0 -lt $(( $(($y-1)) * $sq_len)) ]]
 	then
 		neigs[0]=-1
 	else
 		neigs[0]=$n0
+	fi
 
 	local n1=$((index-$sq_len))
 	if [ $n1 -lt 0 ]
@@ -41,13 +45,15 @@ find_neig_sphere(){
 		neigs[1]=-1
 	else
 		neigs[1]=$n1
+	fi
 
 	local n2=$((index-$sq_len+1))
-	if [ [ $n2 -le 0 ] || [ $n2 -gt $(( $(($y - 1))  )) ]
+	if [[ $n2 -lt 0 || $n2 -gt $(( $(($y * $sq_len))-1 )) ]]
 	then
 		neigs[2]=-1
 	else
 		neigs[2]=$n2
+	fi
 	
 	local n3=$((index-1))
 	if [ $n3 -lt $(($y * $sq_len)) ]
@@ -55,6 +61,7 @@ find_neig_sphere(){
 		neigs[3]=-1
 	else
 		neigs[3]=$n3
+	fi
 	
 	local n4=$((index+1))
 	if [ $n4 -gt $(( $(( $(($y+1))*$sq_len )) -1 )) ]
@@ -62,13 +69,15 @@ find_neig_sphere(){
 		neigs[4]=-1
 	else
 		neigs[4]=$n4
+	fi
 	
 	local n5=$((index+$sq_len-1))
-	if [ [ $n5 -lt $(( $(($y+1)) * $sq_len )) ] || [ $n5 -ge $len_grid ] ]
+	if [[ $n5 -lt $(( $(($y+1)) * $sq_len )) || $n5 -ge $len_grid ]]
 	then
 		neigs[5]=-1
 	else
 		neigs[5]=$n5
+	fi
 		
 	local n6=$((index+$sq_len))
 	if [ $n5 -ge $len_grid ]
@@ -76,13 +85,15 @@ find_neig_sphere(){
 		neigs[6]=-1
 	else
 		neigs[6]=$n6
+	fi
 	
 	local n7=$((index+$sq_len+1))
-	if [ [ $n7 -gt $(( $(( $(($y+2))*$sq_len )) -1 )) ] || [ $n5 -ge $len_grid ] ]
+	if [[ $n7 -gt $(( $(( $(($y+2))*$sq_len )) -1 )) || $n5 -ge $len_grid ]]
 	then
 		neigs[7]=-1
 	else
 		neigs[7]=$n7
+	fi
 	
-	echo "$neigs"
+	echo ${neigs[*]}
 }
