a28cb13f60512d26569be2c57cab8b1ea693832d
Léopold <leopold.chappuis@etu.utc.fr>
2022-07-08 11:39:25 +0200

    ajout variable locale sur la position

--- jalons.sh
+++ jalons.sh
@@ -9,12 +9,12 @@ jalon() {
 	if [ -z $1 ]; then 
 		echo "Error Code 01 : Veuillez ajouter un argument de position à jalon"
 	else
-		position=$1
+		local positionjal=$1
 	fi
 	
 	#Tableau affichage / nom à modifier pour agir sur une variable globale
 	
-	grid_af[$position]="¤"
+	grid_af[$positionjal]="¤"
 	
 }
 
