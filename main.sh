53e73ddc775dd273cde173f3b64cd137f92d9885
Léopold <leopold.chappuis@etu.utc.fr>
2022-07-08 16:38:58 +0200

    Ajout des fonctions dans le fichier main

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ source demarrage.sh
 source getkey.sh
 source affichage_grille.sh
 source grille_depart.sh
+source generation_aleatoire.sh
 
 emplacement=0
 taille_grille=8
@@ -15,7 +16,12 @@ fin=0
 function main () {
 	
 	#Appel de la fonction démarrage 
+	
+	randomGenerate 40 "$taille_grille"
+	numbersDetection 
+
 	choisir_menu
+
 	
 	while [ $fin -eq 0 ]; do
 		clear
